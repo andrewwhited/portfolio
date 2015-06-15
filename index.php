b9d6171b89388f9fd1fe7ab5e1a7c07809d9c24c
andrewwhited <andrewwhited@Andrew-Whited-2.local>
2015-06-15 13:39:09 -0500

    index done and responsive nav

--- index.php
+++ index.php
@@ -2,14 +2,14 @@
 
 <div class="container scene_element fadein">
 	<div class="row">
-		<div class="half column">
+		<div class="column header">
 			<a href="index.php" class="logo"><?php include('images/logo.php'); ?></a>
+			<nav>
+				<a href="thoughts.php">Thoughts</a>
+				<a href="work.php">Work</a>
+				<a href="about.php">About</a>
+			</nav>
 		</div><!--End column -->
-		<nav class="half column">
-			<a href="thoughts.php">Thoughts</a>
-			<a href="work.php">Work</a>
-			<a href="about.php">About</a>
-		</nav><!--End column -->
 	</div><!--End Row -->
 	<div class="row">
 		<div class="column big">

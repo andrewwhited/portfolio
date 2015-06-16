9a2da3b181da45fcc6efe63e3adb5cb258d02b4d
andrewwhited <andrewwhited@Andrew-Whited-2.local>
2015-06-16 17:18:19 -0500

    worked on about page

--- about.php
+++ about.php
@@ -1,18 +1,54 @@
 <?php include('header.php');?>
 
-
-<!-- About
+<!-- Hero Section
 –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-<div id="main">
-  <div class="container">
+<div class="container scene_element fadein">
+	<div class="row">
+		<div class="column header">
+			<a href="index.php" class="logo"><?php include('images/logo.php'); ?></a>
+		</div><!--End column -->
+	</div><!--End Row -->
     <div class="row">
-      <div class="six columns offset-by-one">
-        <h1>About</h1>
-        <p>Text about stuff here probably</p>
-      </div>
-    </div>
+	    <div class="column photo">
+	    	<img src="images/me.jpg">
+	    </div><!--End Column-->
+  	</div><!--End Row  ===============-->
+</div><!--End Hero Container-->
+
+<!-- Content Section
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+<div class="container">
+  <div class="row aplha">
+    <div class="half column text">
+      <h3>Who am I?</h3>
+      <p>Woah, that is a pretty heavy question. About me bio type stuff can go here.Woah, that is a pretty heavy question. About me bio type stuff can go here.Woah, that is a pretty heavy question. About me bio type stuff can go here.Woah, that is a pretty heavy question. About me bio type stuff can go here.Woah, that is a pretty heavy question. About me bio type stuff can go here.Woah, that is a pretty heavy question. About me bio type stuff can go here.Woah, that is a pretty heavy question. About me bio type stuff can go here.</p>
+    </div><!--End column -->
+    <div class="half column contact">
+    	<div class="info item">
+    		<h2><a href="#">andrewgwhited<wbr>@gmail.com</a></h2>
+      		<h2>+1 (770) 329 3560</h2>
+    	</div>
+    </div><!--End Column-->
+  </div><!--End Row ===============-->
+  <div class="row omega">
+  	<div class="half column text">
+      <h3>Who am I?</h3>
+      <p>Woah, that is a pretty heavy question. About me bio type stuff can go here.Woah, that is a pretty heavy question. About me bio type stuff can go here.Woah, that is a pretty heavy question. About me bio type stuff can go here.Woah, that is a pretty heavy question. About me bio type stuff can go here.Woah, that is a pretty heavy question. About me bio type stuff can go here.Woah, that is a pretty heavy question. About me bio type stuff can go here.Woah, that is a pretty heavy question. About me bio type stuff can go here.</p>
+      <span class="therefore">&there4;</span><!--Ending Tombstone-->
+    </div><!--End column -->
+    <div class="half column places">
+    	<a href="http://www.ibm.com/design/" target="_blank" class="ibm"><?php include('images/IBM.php'); ?></a>
+    	<a href="http://austin.aiga.org/" target="_blank" class="aiga"><?php include('images/AIGA.php'); ?></a>
+    	<a href="http://www.iconologic.com/" target="_blank" class="icon"><?php include('images/IBM.php'); ?></a>
+    	<a href="http://digd.auburn.edu/graphicdesign/" target="_blank" class="gdes"><?php include('images/GDES.php'); ?></a>
+    </div><!--End column -->
+  </div><!--End Row ===============-->
+  <div class="row">
+  	<div class="column resume">
+    	<a href="#"><h2>Download Resume</h2></a>
+    </div><!--End Column-->
   </div>
-</div>
+</div><!--End Content Container-->
 
 <!-- End Document
   –––––––––––––––––––––––––––––––––––––––––––––––––– -->

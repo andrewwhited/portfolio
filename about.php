8bc5db29b43b34f1ec5b2c12a96dc3f9318e6fd6
andrewwhited <andrewgwhited@gmail.com>
2015-06-19 16:24:24 -0500

    about rough done

--- about.php
+++ about.php
@@ -21,7 +21,7 @@
   <div class="row aplha">
     <div class="half column text">
       <h3>Who am I?</h3>
-      <p>Woah, that is a pretty heavy question. About me bio type stuff can go here.Woah, that is a pretty heavy question. About me bio type stuff can go here.Woah, that is a pretty heavy question. About me bio type stuff can go here.Woah, that is a pretty heavy question. About me bio type stuff can go here.Woah, that is a pretty heavy question. About me bio type stuff can go here.Woah, that is a pretty heavy question. About me bio type stuff can go here.Woah, that is a pretty heavy question. About me bio type stuff can go here.</p>
+      <p>That is a surprisingly tough question to answer. <em>Who am I?</em> You could say I am an <a target="blank" href="https://en.wikipedia.org/?title=INTP">INTP</a> or a <a target="blank" href="https://en.wikipedia.org/wiki/Enneagram_of_Personality">3 wing 7</a>, but that sounds too sterile. These little stats surely say something about me, yet they leave a lot out. The part that you probably care about is that I am a designer. At different times in my journey being a designer has meant pulling screenprints, stressing over type, interviewing people, debating with software architects, setting up studio lighting, and writing code. I am sure it will mean a lot of other things to me as I move forward. But that is the beauty of design, isn't it?<br><br><a href="#">See my résumé.</a></p>
     </div><!--End column -->
     <div class="half column contact">
     	<div class="info item">
@@ -30,24 +30,13 @@
     	</div>
     </div><!--End Column-->
   </div><!--End Row ===============-->
-  <div class="row omega">
-  	<div class="half column text">
-      <h3>Who am I?</h3>
-      <p>Woah, that is a pretty heavy question. About me bio type stuff can go here.Woah, that is a pretty heavy question. About me bio type stuff can go here.Woah, that is a pretty heavy question. About me bio type stuff can go here.Woah, that is a pretty heavy question. About me bio type stuff can go here.Woah, that is a pretty heavy question. About me bio type stuff can go here.Woah, that is a pretty heavy question. About me bio type stuff can go here.Woah, that is a pretty heavy question. About me bio type stuff can go here.</p>
-      <span class="therefore">&there4;</span><!--Ending Tombstone-->
-    </div><!--End column -->
-    <div class="half column places">
+  <div class="column places">
     	<a href="http://www.ibm.com/design/" target="_blank" class="ibm"><?php include('images/IBM.php'); ?></a>
     	<a href="http://austin.aiga.org/" target="_blank" class="aiga"><?php include('images/AIGA.php'); ?></a>
     	<a href="http://www.iconologic.com/" target="_blank" class="icon"><?php include('images/ICON.php'); ?></a>
     	<a href="http://digd.auburn.edu/graphicdesign/" target="_blank" class="gdes"><?php include('images/GDES.php'); ?></a>
     </div><!--End column -->
   </div><!--End Row ===============-->
-  <div class="row">
-  	<div class="column resume">
-    	<a href="#"><h2>Download Resume</h2></a>
-    </div><!--End Column-->
-  </div>
 </div><!--End Content Container-->
 
 <!-- End Document

c9c1f6a3e1c4dd94f31c9224981cdc6b4173c747
andrewwhited <andrewgwhited@gmail.com>
2015-07-06 11:47:30 -0500

    starting to flesh out MARTA

--- project.php
+++ project.php
@@ -6,7 +6,7 @@
   <div class="row">
     <div class="half column title scene_element slidedown">
       <a href="index.php" class="logo"><?php include('images/logo.php'); ?></a>
-      <h1>Color Blind</br>Research</br>Foundation</h1>
+      <h1>MARTA</h1>
     </div><!--End column -->
     <div class="half column title photo">
       <img 
@@ -25,8 +25,168 @@
 <div class="container">
   <div class="row">
     <div class="half column text">
-      <h3>Subject</h3>
-      <p>Since this was an open ended project, I wanted to pick a subject that I would enjoy spending time researching and learning about. The only requirements were to make up a company, create a logo, stationary set, and a series of ads. I have been interested in how we perceive colors and light ever since I picked up Josef Albers’s <a href="http://www.amazon.com/Interaction-Color-Revised-Expanded-Edition/dp/0300115954/ref=pd_sim_14_2?ie=UTF8&refRID=15Y4S60VRM0MX9PVV2YC"><em>Interaction of Color</em>.</a> Since I am affected by a mild form of color blindness, I thought that would be an interesting domain to dive into.</p>
+      <h3>The rise of MARTA</h3>
+      <p>MARTA (Metropolitan Atlanta Rapid Transit Authority) is the primary public transportation system in Atlanta. As the 8th largest transportation service in the nation, MARTA transports over 420,000 passengers a day [wiki]. MARTA has both a subway system and a bus system that operate between 38 rail stations and over 100 bus lines.</p>
+      <p>Former Atlanta Mayor Sam Massell campaigned for MARTA and helped bring rapid transportation to the city. MARTA was the envy of other cities when it was initiated in 1972 and promised to connect Atlantans to the whole of their city, both work and play. The birth of MARTA represented a milestone for technology and development in Atlanta. </p>
+    </div><!--End column -->
+    <div class="half column photo">
+               <img 
+              srcset="images/colorBlind/14XL.jpg   4288w,
+                      images/colorBlind/14L.jpg   1920w,
+                      images/colorBlind/14M.jpg  1200w,
+                      images/colorBlind/14S.jpg  600w"
+              sizes="100vw"
+              src="images/colorBlind/14M.jpg"
+              alt="" />
+    </div><!--End Column-->
+  </div><!--End Row  ===============-->
+  <div class="row pull">
+    <div>
+      <h4 class="quote">“The main goal of MARTA was mobility; mobility is man's fifth freedom. [The lack of mobility] imprisons people in their neighborhoods.”</h4>
+      <h5><a href="https://en.wikipedia.org/wiki/Sam_Massell">— Sam Massell</a></h5>
+    </div><!--End column -->
+  </div><!--End Row  ===============-->
+  <div class="row">
+    <div class="half column text">
+      <h3>What went wrong?</h3>
+      <p>Once a source of and awe pride for Atlanta, MARTA is now unfortunately more closely associated with crime and lack of upkeep to some. MARTA's interaction with its customers is hindered by dated aesthetics, inconsistent design, and ineffective and unclear communication.</p>        
+      <p>MARTA feels far from modern. Its inconsistent typography illegible wayfinding, and clunky website and app devalue its brand.</p>
+    </div><!--End column -->
+    <div class="half column photo">
+      <img 
+        src="images/colorBlind/3.svg"
+        alt="Diagram" />
+    </div><!--End Column-->
+  </div><!--End Row  ===============-->
+  <div class="row">
+    <div class="column half photos">
+            <img 
+              srcset="images/colorBlind/4XL.jpg   2700w,
+                      images/colorBlind/4L.jpg   1920w,
+                      images/colorBlind/4M.jpg  1200w,
+                      images/colorBlind/4S.jpg  600w"
+              sizes="100vw"
+              src="images/colorBlind/4M.jpg"
+              alt="Bus Stop Ad"/>
+    </div><!--End Column-->
+    <div class="column half photos">
+        <img 
+          srcset="images/colorBlind/5XL.jpg   2700w,
+                  images/colorBlind/5L.jpg   1920w,
+                  images/colorBlind/5M.jpg  1200w,
+                  images/colorBlind/5S.jpg  600w"
+          sizes="100vw"
+          src="images/colorBlind/5M.jpg"
+          alt="Bus Stop Ad"/>
+    </div><!--End Column-->
+  </div><!--End Row  ===============-->
+  <div class="row">
+    <div class="half column text">
+      <h3>Who is MARTA for?</h3>
+      <p>MARTA is a large system that interacts with an equally large demographic. The market can be segmented by frequency of use, time and direction of use, and destination. The three market slices that account for the majority of transit use are Workers, Students, and Occasional Users.</p>
+      <h3>Workers</h3>
+      <ul>
+        <li>45% of rides</li>
+        <li>Consistant everyday use</li>
+        <li>Limited alteratives</li>
+        <li>Riding into the city from suburbs</li>
+      </ul>
+      <h3>Students</h3>
+      <ul>
+        <li>31% of rides</li>
+        <li>2-4 days a week</li>
+        <li>Travel within downtown</li>
+      </ul>
+      <h3>Event Users</h3>
+      <ul>
+        <li>Infrequent and inconsistant ridership</li>
+        <li>Off-peak hours</li>
+        <li>Have an alternate option</li>
+        <li>Going to airport, Braves games, etc.</li>
+      </ul>
+    </div><!--End column -->
+    <div class="half column photo">
+      <img 
+        src="images/colorBlind/9.svg"
+        alt="" />  
+    </div><!--End Column-->
+  </div><!--End Row  ===============-->
+  <div class="row">
+    <div class="column photo">
+            <img 
+              srcset="images/colorBlind/4XL.jpg   2700w,
+                      images/colorBlind/4L.jpg   1920w,
+                      images/colorBlind/4M.jpg  1200w,
+                      images/colorBlind/4S.jpg  600w"
+              sizes="100vw"
+              src="images/colorBlind/4M.jpg"
+              alt="Bus Stop Ad"/>
+    </div><!--End Column-->
+  </div><!--End Row  ===============-->
+  <div class="row">
+    <div class="half column text">
+      <h3>Opportunities</h3>
+      <p>Some problems with MARTA are operational (the inevitable reduction of service and increase in fares due to economic restrictions), but by using clear, consistent, and engaging design throughout its touch points with riders MARTA can increase its ridership and its quality of user experience.</p>        
+    </div><!--End column -->
+    <div class="half column photo">
+      <img 
+        src="images/colorBlind/3.svg"
+        alt="Diagram" />
+    </div><!--End Column-->
+  </div><!--End Row  ===============-->
+  <div class="row pull">
+    <div>
+      <h4>Mobility allows Atlantians to experience Atlanta to its fullest.</h4>
+    </div><!--End column -->
+  </div><!--End Row  ===============-->
+  <div class="row">
+    <div class="half column text">
+      <h3>Typeface</h3>
+      <p>One of the major problems I had when using MARTA was that the type was very illegible from distances and angles, so I wanted to address this first in my redesign. Marta’s current typeface does not meet the standards laid out in the ADA. I tested different typefaces legibility by hanging printouts at the end of our hall and getting classmates to try to read them. I found that Gotham preforms well from an accessibility standpoint and appropriately communicates the proposed MARTA brand.</p>
+    </div><!--End column -->
+    <div class="half column photo">
+               <img 
+              srcset="images/colorBlind/14XL.jpg   4288w,
+                      images/colorBlind/14L.jpg   1920w,
+                      images/colorBlind/14M.jpg  1200w,
+                      images/colorBlind/14S.jpg  600w"
+              sizes="100vw"
+              src="images/colorBlind/14M.jpg"
+              alt="" />
+    </div><!--End Column-->
+  </div><!--End Row  ===============-->
+  <div class="row">
+    <div class="column half photos">
+            <img 
+              srcset="images/colorBlind/4XL.jpg   2700w,
+                      images/colorBlind/4L.jpg   1920w,
+                      images/colorBlind/4M.jpg  1200w,
+                      images/colorBlind/4S.jpg  600w"
+              sizes="100vw"
+              src="images/colorBlind/4M.jpg"
+              alt="Bus Stop Ad"/>
+    </div><!--End Column-->
+    <div class="column half photos">
+        <img 
+          srcset="images/colorBlind/5XL.jpg   2700w,
+                  images/colorBlind/5L.jpg   1920w,
+                  images/colorBlind/5M.jpg  1200w,
+                  images/colorBlind/5S.jpg  600w"
+          sizes="100vw"
+          src="images/colorBlind/5M.jpg"
+          alt="Bus Stop Ad"/>
+    </div><!--End Column-->
+  </div><!--End Row  ===============-->
+  <div class="row pull">
+  <div>
+    <h4 class="quote">A typog­ra­pher who hasn’t found the appro­pri­ate typeface may not have decreased the informational value of a text, but gave up the oppor­tu­nity to consid­er­ably increase its effectiveness.</h4>
+    <h5><a href="https://books.google.com/books/about/Legibility_Atmosphere_value_and_Forms_of.html?id=RZzgAAAAMAAJ">— G.W. Ovink</a></h5>
+  </div><!--End column -->
+</div><!--End Row  ===============-->
+<div class="row">
+    <div class="half column text">
+      <h3>Logo</h3>
+      <p>Arguably, the most iconic part of a brand is the logo. It is the most explicit in identifying a company. I started with the idea that MARTA consists of two interconnected systems. The concepts of mobility, trust, movement, and modernity were important to the branding. </p>
     </div><!--End column -->
     <div class="half column photo">
                <img 
@@ -42,81 +202,72 @@
   <div class="row">
     <div class="column photo">
             <img 
-              srcset="images/colorBlind/2XL.gif   2880w,
-                      images/colorBlind/2L.gif   1920w,
-                      images/colorBlind/2M.gif  1200w,
-                      images/colorBlind/2S.gif  600w"
+              srcset="images/colorBlind/4XL.jpg   2700w,
+                      images/colorBlind/4L.jpg   1920w,
+                      images/colorBlind/4M.jpg  1200w,
+                      images/colorBlind/4S.jpg  600w"
               sizes="100vw"
-              src="images/colorBlind/2M.gif"
-              alt="Ishihara Test Pattern" />
+              src="images/colorBlind/4M.jpg"
+              alt="Bus Stop Ad"/>
     </div><!--End Column-->
   </div><!--End Row  ===============-->
   <div class="row">
     <div class="half column text">
-      <h3>Vision</h3>
-      <p>I started by checking out as many books on vision and light reception as I could. I found books on everything from the physics of light, to Ophthalmology, and even a really old set of <a href="https://en.wikipedia.org/wiki/Ishihara_Test">Ishihara plates.</a> I started to learn about the types of color deficiencies and how the anatomy of the eye affect our perception of light. </p>        
-      <h3>Logo</h3>
-      <p>I explored quite a few options, but the direction that I thought worked best was one that hinted at some of the medical illustrations that I had seen. I wanted to find a way to actually represent the idea of color blindness in a simple way. In the logo I depicted light entering the eye as color, but hitting the retina as grey. </p>
+      <h3>Icons</h3>
+      <p>Arguably, the most iconic part of a brand is the logo. It is the most explicit in identifying a company. I started with the idea that MARTA consists of two interconnected systems. The concepts of mobility, trust, movement, and modernity were important to the branding. </p>
     </div><!--End column -->
     <div class="half column photo">
-      <img 
-        src="images/colorBlind/3.svg"
-        alt="Diagram" />
+               <img 
+              srcset="images/colorBlind/14XL.jpg   4288w,
+                      images/colorBlind/14L.jpg   1920w,
+                      images/colorBlind/14M.jpg  1200w,
+                      images/colorBlind/14S.jpg  600w"
+              sizes="100vw"
+              src="images/colorBlind/14M.jpg"
+              alt="" />
     </div><!--End Column-->
   </div><!--End Row  ===============-->
   <div class="row">
     <div class="column photo">
             <img 
-              srcset="images/colorBlind/10XL.jpg   3931w,
-                      images/colorBlind/10L.jpg   1920w,
-                      images/colorBlind/10M.jpg  1200w,
-                      images/colorBlind/10S.jpg  600w"
+              srcset="images/colorBlind/4XL.jpg   2700w,
+                      images/colorBlind/4L.jpg   1920w,
+                      images/colorBlind/4M.jpg  1200w,
+                      images/colorBlind/4S.jpg  600w"
               sizes="100vw"
-              src="images/colorBlind/10M.jpg"
-              alt="" />
-    </div><!--End Column-->
-  </div><!--End Row  ===============-->
-  <div class="row">
-    <div class="column half photos">
-            <img 
-              srcset="images/colorBlind/11XL.jpg   4288w,
-                      images/colorBlind/11L.jpg   1920w,
-                      images/colorBlind/11M.jpg  1200w,
-                      images/colorBlind/11S.jpg  600w"
-              sizes="100vw"
-              src="images/colorBlind/11M.jpg"
-              alt="" />
-    </div><!--End Column-->
-    <div class="column half photos">
-        <img 
-          srcset="images/colorBlind/12XL.jpg   4288w,
-                  images/colorBlind/12L.jpg   1920w,
-                  images/colorBlind/12M.jpg  1200w,
-                  images/colorBlind/12S.jpg  600w"
-          sizes="100vw"
-          src="images/colorBlind/12M.jpg"
-          alt="" />
+              src="images/colorBlind/4M.jpg"
+              alt="Bus Stop Ad"/>
     </div><!--End Column-->
   </div><!--End Row  ===============-->
   <div class="row">
     <div class="half column text">
-      <h3>Ads</h3>
-      <p>I took the stance that if vision is a fundamental way of perceiving the world, then color blindness was an issue in perception. I wanted something that could be seen in multiple ways that was somehow related to color. I coincidentally was reminded of some old GI Joe Decoder graphics that used a red film to reveal a message. By matching the color of a film and printed ink, you can cancel out some of the image and enhance other bits. I had been playing with the idea of doing bus stop ads and trying to incorporate more of the context into the ads; the glass walls of a bus stop provided the perfect structure to try out this effect. I chose contradictory idioms to overlay and then spent a ton of time calibrating the colors needed by trial and error. The result was an ad whose message was quite different depending on how color interacted with it.</p>
-      <span class="therefore">&there4;</span><!--Ending Tombstone-->
+      <h3>Wayfinding</h3>
+      <p>The main way that users interact with the design is though signage. The way finding for a major transportation system has a lot of functional requirements, architectural restrictions, and accessibility rules. The main responsibility of way finding signage is to help riders when they reach a decision point. A decision point could be using an elevator, going to a train platform, or transferring to a bus. I created flow charts to map the paths that a rider could take in order to find out where and what type of signs were needed. Each station has a different layout, so I rode around and documented studied the different stations.</p>
     </div><!--End column -->
     <div class="half column photo">
-      <img 
-        src="images/colorBlind/9.svg"
-        alt="" />  
+               <img 
+              srcset="images/colorBlind/14XL.jpg   4288w,
+                      images/colorBlind/14L.jpg   1920w,
+                      images/colorBlind/14M.jpg  1200w,
+                      images/colorBlind/14S.jpg  600w"
+              sizes="100vw"
+              src="images/colorBlind/14M.jpg"
+              alt="" />
     </div><!--End Column-->
   </div><!--End Row  ===============-->
-  <div class="row pull">
-    <div>
-      <h4 class="quote">“It is a narrow mind which cannot look at a subject from various points of view.”</h4>
-      <h5><a href="https://www.gutenberg.org/files/145/145-h/145-h.htm">— George Eliot</a></h5>
-    </div><!--End column -->
-  </div><!--End Row  ===============-->
   <div class="row">
+    <div class="column photo">
+            <img 
+              srcset="images/colorBlind/4XL.jpg   2700w,
+                      images/colorBlind/4L.jpg   1920w,
+                      images/colorBlind/4M.jpg  1200w,
+                      images/colorBlind/4S.jpg  600w"
+              sizes="100vw"
+              src="images/colorBlind/4M.jpg"
+              alt="Bus Stop Ad"/>
+    </div><!--End Column-->
+  </div><!--End Row  ===============-->
+    <div class="row">
     <div class="column half photos">
             <img 
               srcset="images/colorBlind/4XL.jpg   2700w,
@@ -125,7 +276,7 @@
                       images/colorBlind/4S.jpg  600w"
               sizes="100vw"
               src="images/colorBlind/4M.jpg"
-              alt="Bus Stop Ad" />
+              alt="Bus Stop Ad"/>
     </div><!--End Column-->
     <div class="column half photos">
         <img 
@@ -135,39 +286,7 @@
                   images/colorBlind/5S.jpg  600w"
           sizes="100vw"
           src="images/colorBlind/5M.jpg"
-          alt="Bus Stop Ad" />
-    </div><!--End Column-->
-  </div><!--End Row  ===============-->
-  <div class="row">
-    <div class="third column photos">
-            <img 
-              srcset="images/colorBlind/6XL.jpg   2890,
-                      images/colorBlind/6L.jpg   1920w,
-                      images/colorBlind/6M.jpg  1200w,
-                      images/colorBlind/6S.jpg  600w"
-              sizes="100vw"
-              src="images/colorBlind/6M.jpg"
-              alt="Color Blind Research Foundation Ad" />
-    </div><!--End Column-->
-    <div class="third column photos">
-            <img 
-              srcset="images/colorBlind/7XL.jpg   2890,
-                      images/colorBlind/7L.jpg   1920w,
-                      images/colorBlind/7M.jpg  1200w,
-                      images/colorBlind/7S.jpg  600w"
-              sizes="100vw"
-              src="images/colorBlind/7M.jpg"
-              alt="Color Blind Research Foundation Ad" />
-    </div><!--End Column-->
-    <div class="third column photos">
-            <img 
-              srcset="images/colorBlind/8XL.jpg   2890,
-                      images/colorBlind/8L.jpg   1920w,
-                      images/colorBlind/8M.jpg  1200w,
-                      images/colorBlind/8S.jpg  600w"
-              sizes="100vw"
-              src="images/colorBlind/8M.jpg"
-              alt="Color Blind Research Foundation Ad" />
+          alt="Bus Stop Ad"/>
     </div><!--End Column-->
   </div><!--End Row  ===============-->
 </div><!--End Content Container-->

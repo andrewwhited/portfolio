8a9b4dc071475e5de5fc3c1aa197a25bdc3a90e9
andrewwhited <andrewgwhited@gmail.com>
2015-07-13 18:59:25 -0500

    Impact Alloy done

--- impactAlloy.php
+++ impactAlloy.php
@@ -22,76 +22,75 @@
   <div class="row">
     <div class="half column text">
       <h3>Objective</h3>
-      <p>This poster was part of a study on <a href="http://en.wikipedia.org/wiki/Semiotics">semiotics.</a> I think the function of communication is an interesting and important topic for designers. The aim of this project was to explore the various levels of meaning surrounding a word and to create a poster that represented my findings. This poster was a way to frame my research; it exists as more of a study than a traditionally functional piece of design. As a catalyst, I picked a word from a hat to use for the project.</p>
+      <p>Impact Alloy is a fictitious knife company that sells hand-crafted pocket knives constructed from meteoric iron. Meteors are the only natural source of pure iron and were humans first source of iron for steel. I wanted to create a brand that would communicate the unique and expensive qualities of the knife.</p>
     </div><!--End column -->
     <div class="half column photo">
-      <img src="images/grade/5.jpg">
+        <img 
+        srcset="images/impactAlloy/7XL.jpg   2560w,
+                images/impactAlloy/7L.jpg   1920w,
+                images/impactAlloy/7M.jpg  1200w,
+                images/impactAlloy/7S.jpg  600w"
+        sizes="(min-width: 800px) 50vw, 100vw"
+        src="images/impactAlloy/7M.jpg"
+        alt="Meteor" />
     </div><!--End Column-->
   </div><!--End Row ===============-->
   <div class="row pull">
     <div>
-      <h4>The word I picked was 'Grade'</h4>
+      <h4 class="quote">“Now slides the silent meteor on, and leaves</br>A shining furrow, as thy thoughts in me.”</h4>
+      <h5><a href="http://www.poetryfoundation.org/poem/174647">— Alfred, Lord Tennyson</a></h5>
     </div>
   </div><!--End Row  ===============-->
   <div class="row">
     <div class="half column text">
       <h3>Logo</h3>
-      <p>The term grade can be used in many contexts as both a verb or a noun. The first use that came to mind for me was the grading systems that rate the difficulty of rock climbs. The word grade also has specific meanings in the fields of education, engineering, mathematics, and healthcare among others. By looking into the different applications of the word I was able to synthesize my own definition:</p>        
+      <p>In my research, I was drawn more to luxury brands than other knife companies. The material of the knife positions it more for opulence than utility. I experimented with how to combine the form of a meteor and a reference to cutting. I took a geometric approach to the meteor and its tail and used a sharp notch and the profile of the mark to hint at a knife. </p>        
     </div><!--End column -->
     <div class="half column photo">
-      <img src="images/impactAlloy/5.svg">
+      <img src="images/impactAlloy/5.svg"
+      alt="Ompact Alloy Logo Diagram">
     </div><!--End Column-->
-  </div><!--End Row  ===============-->
-  <div class="row pull">
-    <div>
-      <h4>The act of quantifying or qualifying an object in relation to its peers in order to make a usable ranking.</h4>
-    </div>
   </div><!--End Row  ===============-->
   <div class="row">
-    <div class="half column text">
-      <h3>Denotation</h3>
-      <p>Looking at the examples of grading were helpful when I was coming up with a definition, but they did’t provide a very helpful visual language to use for the poster. I decided to reevaluate the problem and the approach that I had taken so far by instead looking at the core act of grading. What does the concept of grading look like? I decided that the best way to represent grading was to demonstrate it on a set of objects. I needed something that was unique enough to distinguish between, yet was neutral enough to not distract from the concept. While walking to class, I realized that the gravel I was standing on would be perfect. </p>
-    </div><!--End column -->
-    <div class="half column photo">
-      <img 
-        srcset="images/grade/3XL.jpg   3500w,
-                images/grade/3L.jpg   1920w,
-                images/grade/3M.jpg  1200w,
-                images/grade/3S.jpg  600w"
-        sizes="(min-width: 800px) 50vw, 100vw"
-        src="images/grade/3M.jpg"
-        alt="Grade Poster" />
-    </div><!--End Column-->
-  </div><!--End Row  ===============-->
-      <div class="row">
     <div class="column photo">
-            <img 
-              srcset="images/grade/2XL.jpg   4185w,
-                      images/grade/2L.jpg   1920w,
-                      images/grade/2M.jpg  1200w,
-                      images/grade/2S.jpg  600w"
-              sizes="100vw"
-              src="images/grade/2M.jpg"
-              alt="Grade Poster" />
+      <img 
+        srcset="images/impactAlloy/4XL.jpg   4258w,
+                images/impactAlloy/4L.jpg   1920w,
+                images/impactAlloy/4M.jpg  1200w,
+                images/impactAlloy/4S.jpg  600w"
+        sizes="(min-width: 800px) 50vw, 100vw"
+        src="images/impactAlloy/4M.jpg"
+        alt="Impact Alloy Logo Card" />
     </div><!--End Column-->
   </div><!--End Row  ===============-->
   <div class="row">
     <div class="half column text">
-      <h3>Process</h3>
-      <p>At this point the process was pretty straight forward: Scoop up a random selection of rocks, order them based on size, arrange the rocks in a grid, and photograph them. </p>
-      <p>In order to best present and showcase the concept, I wanted the poster to feel structured and quiet. To accomplish this, I took a formal approach to the page shape, spacing, and type by strictly implementing a ratio to create all of my measurements. I decided to light the rocks in a way that would highlight their individual details and bring out their differences.</p>
+      <h3>Packaging</h3>
+      <p>Knife packaging can range from a clear plastic pouch to leather holsters to etched wood cases. I created a box with a fabric inset to house the knife and a card talking about its properties. The size of the box gives it more presence and coincides with it being a collector’s item.</p>
       <span class="therefore">&there4;</span><!--Ending Tombstone-->
     </div><!--End column -->
     <div class="half column photo">
       <img 
-        srcset="images/grade/4XL.jpg   4288w,
-                images/grade/4L.jpg   1920w,
-                images/grade/4M.jpg  1200w,
-                images/grade/4S.jpg  600w"
+        srcset="images/impactAlloy/3XL.jpg   4288w,
+                images/impactAlloy/3L.jpg   1920w,
+                images/impactAlloy/3M.jpg  1200w,
+                images/impactAlloy/3S.jpg  600w"
         sizes="(min-width: 800px) 50vw, 100vw"
-        src="images/grade/4M.jpg"
-        alt="Grade Poster" />  
+        src="images/impactAlloy/3M.jpg"
+        alt="Impact Alloy Knife" />  
     </div><!--End Column-->
+  </div><!--End Row  ===============-->
+  <div class="row">
+  <div class="column photo">
+    <img 
+      srcset="images/impactAlloy/2XL.jpg   4288w,
+              images/impactAlloy/2L.jpg   1920w,
+              images/impactAlloy/2M.jpg  1200w,
+              images/impactAlloy/2S.jpg  600w"
+      sizes="100vw"
+      src="images/impactAlloy/2M.jpg"
+      alt="Impact Alloy Box" />
+  </div><!--End Column-->
   </div><!--End Row  ===============-->
 </div><!--End Content Container-->
 

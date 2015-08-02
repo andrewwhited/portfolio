0ce40223f5dee8bb05e03651e5550315981b9589
andrewwhited <andrewgwhited@gmail.com>
2015-08-02 15:07:02 -0500

    finalized Grade Text

--- grade.php
+++ grade.php
@@ -27,7 +27,7 @@
   <div class="row">
     <div class="half column text">
       <h3>Problem</h3>
-      <p>This poster was part of a study on <a href="http://en.wikipedia.org/wiki/Semiotics">semiotics.</a> I think the function of communication is an interesting and important topic for designers. The aim of this project was to explore the various levels of meaning surrounding a word and to create a poster that represented my findings. This poster was a way to frame my research; it exists as more of a study than a traditionally functional piece of design. As a catalyst, I picked a word from a hat to use for the project.</p>
+      <p>This poster was part of a study on <a href="http://en.wikipedia.org/wiki/Semiotics">semiotics.</a> I think the function of communication is an interesting and important topic for designers. The aim of this project was to explore the various levels of meaning surrounding a word and to create a poster that represented my findings. This poster was a way to frame my research; it exists as more of a study than a traditionally functional piece of design. To start I picked a word from a hat to use for the project.</p>
     </div><!--End column -->
     <div class="half column photo">
       <img src="images/grade/5.jpg">
